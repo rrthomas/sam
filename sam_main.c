0f9a7336496fe807844c94108df019e56949909d
Reuben Thomas <rrt@sc3d.org>
2025-10-05 16:21:33 +0100

    sam_main.c: re-indent

--- sam_main.c
+++ sam_main.c
@@ -14,18 +14,18 @@
 #define VERSION "0.1"
 #define PACKAGE_BUGREPORT "rrt@sc3d.org"
 
-#define SAM_COPYRIGHT_STRING \
-  "Copyright (C) 2020 Reuben Thomas."
-#define SAM_VERSION_STRING \
+#define SAM_COPYRIGHT_STRING                    \
+    "Copyright (C) 2020 Reuben Thomas."
+#define SAM_VERSION_STRING                      \
     PACKAGE_NAME " " VERSION
 
 /* Options table */
 struct option longopts[] = {
-#define O(longname, arg, argstring, docstring) \
-  {longname, arg, NULL, '\0'},
+#define O(longname, arg, argstring, docstring)  \
+    {longname, arg, NULL, '\0'},
 #include "tbl_opts.h"
 #undef O
-  {0, 0, 0, 0}
+    {0, 0, 0, 0}
 };
 
 int main(int argc, char *argv[]) {
@@ -48,42 +48,42 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-      switch (longindex) {
-      case 0:
-          do_debug = true;
-          break;
-      case 1:
-          screen_pbm_file = optarg;
-          break;
-      case 2:
-          {
+        switch (longindex) {
+        case 0:
+            do_debug = true;
+            break;
+        case 1:
+            screen_pbm_file = optarg;
+            break;
+        case 2:
+            {
 #define OPT_COLUMN_WIDTH 24
-              char buf[OPT_COLUMN_WIDTH + 1];
-              printf("Usage: %s [OPTION]...\n"
-                     "\n"
-                     "Run SAM, the Super-Awesome Machine.\n"
-                     "\n",
-                     argv[0]);
-#define O(longname, arg, argstring, docstring)              \
-              sprintf(buf, "--%s %s", longname, argstring); \
-              printf("%-24s%s\n", buf, docstring);
+                char buf[OPT_COLUMN_WIDTH + 1];
+                printf("Usage: %s [OPTION]...\n"
+                       "\n"
+                       "Run SAM, the Super-Awesome Machine.\n"
+                       "\n",
+                       argv[0]);
+#define O(longname, arg, argstring, docstring)                  \
+                sprintf(buf, "--%s %s", longname, argstring);   \
+                printf("%-24s%s\n", buf, docstring);
 #include "tbl_opts.h"
 #undef O
-              printf ("\n"
-                      "Report bugs to " PACKAGE_BUGREPORT ".\n");
-              exit (EXIT_SUCCESS);
-          }
-      case 3:
-          printf (SAM_VERSION_STRING "\n"
-                  SAM_COPYRIGHT_STRING "\n"
-                  PACKAGE_NAME " comes with ABSOLUTELY NO WARRANTY.\n"
-                  "You may redistribute copies of " PACKAGE_NAME "\n"
-                  "under the terms of the GNU General Public License.\n"
-                  "For more information about these matters, see the file named COPYING.\n");
-          exit (EXIT_SUCCESS);
-      default:
-          break;
-      }
+                printf ("\n"
+                        "Report bugs to " PACKAGE_BUGREPORT ".\n");
+                exit (EXIT_SUCCESS);
+            }
+        case 3:
+            printf (SAM_VERSION_STRING "\n"
+                    SAM_COPYRIGHT_STRING "\n"
+                    PACKAGE_NAME " comes with ABSOLUTELY NO WARRANTY.\n"
+                    "You may redistribute copies of " PACKAGE_NAME "\n"
+                    "under the terms of the GNU General Public License.\n"
+                    "For more information about these matters, see the file named COPYING.\n");
+            exit (EXIT_SUCCESS);
+        default:
+            break;
+        }
     }
 
     sam_word_t error = SAM_ERROR_OK;
