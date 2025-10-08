756975eea5554204770414821225994b77fea40f
Reuben Thomas <rrt@sc3d.org>
2025-10-08 21:32:13 +0100

    sam_traps.h: make copyright years more accurate

--- sam_traps.h
+++ sam_traps.h
@@ -1,6 +1,6 @@
 // The trap opcodes.
 //
-// (c) Reuben Thomas 1994-2020
+// (c) Reuben Thomas 2020
 //
 // The package is distributed under the GNU Public License version 3, or,
 // at your option, any later version.
