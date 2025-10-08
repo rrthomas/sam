1c3c2401dd5f4675538eff1512072b84cf7311cb
Reuben Thomas <rrt@sc3d.org>
2025-10-08 21:31:25 +0100

    sam_main.c: add copyright notice

--- sam_main.c
+++ sam_main.c
@@ -1,4 +1,12 @@
-// -*- c -*-
+// SAM main routine.
+//
+// (c) Reuben Thomas 1994-2025
+//
+// The package is distributed under the GNU Public License version 3, or,
+// at your option, any later version.
+//
+// THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER’S
+// RISK.
 
 #include <stdbool.h>
 #include <stdio.h>
